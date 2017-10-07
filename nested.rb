bfba6ceac7259cbce35b4602e2c5ff84d219e511
Adam Fudge <coolfudge@gmail.com>
2017-10-07 20:05:22 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -58,7 +58,7 @@ def dennis_ritchies_language
         }
      }
 
-     programmer_hash[:dennis_ritchie][:languages]
+     programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -87,6 +87,7 @@ def adding_matz
 
 programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
 
+programmer_hash[:yukihiro_matsumoto]
 end
 
 def changing_alan
